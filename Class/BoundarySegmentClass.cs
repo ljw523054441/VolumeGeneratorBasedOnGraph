84e1e94565b32a269b49dc4986af579b66ad005d
Garvey Lee <523054441@qq.com>
2021-11-08 20:50:53 +0800

    添加DeepCopy方法，尚未测试

--- Class/BoundarySegmentClass.cs
+++ Class/BoundarySegmentClass.cs
@@ -5,15 +5,20 @@ using System.Collections.Generic;
 
 namespace VolumeGeneratorBasedOnGraph
 {
-    public class BoundarySegment
+    public class BoundarySegment : ICloneable
     {
-        public Line Line;
+        public Line Line { get; set; }
 
-        public string Label;
+        public string Label { get; set; }
 
-        public Point3d From;
-        public Point3d To;
+        public Point3d From { get; set; }
+        public Point3d To { get; set; }
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="segment"></param>
+        /// <param name="label"></param>
         public BoundarySegment(Line segment, string label)
         {
             this.Line = segment;
@@ -23,6 +28,19 @@ namespace VolumeGeneratorBasedOnGraph
             this.To = segment.To;
         }
 
+        /// <summary>
+        /// 利用拷贝构造函数实现深拷贝
+        /// </summary>
+        /// <param name="boundarySegment"></param>
+        public BoundarySegment(BoundarySegment boundarySegment)
+        {
+            
+            this.Label = boundarySegment.Label;
+            this.From = new Point3d(boundarySegment.From);
+            this.To = new Point3d(boundarySegment.To);
+            this.Line = new Line(this.From, this.To);
+        }
+
         public void Reverse()
         {
             Point3d tmp = this.From;
@@ -30,6 +48,19 @@ namespace VolumeGeneratorBasedOnGraph
             this.To = tmp;
 
             this.Line = new Line(this.From, this.To);
+        }
+
+        /// <summary>
+        /// 调用ICloneable接口实现深拷贝
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            BoundarySegment boundarySegment = (BoundarySegment)MemberwiseClone();
+            boundarySegment.From = (Point3d)MemberwiseClone();
+            boundarySegment.To = (Point3d)MemberwiseClone();
+            boundarySegment.Line = new Line(boundarySegment.From, boundarySegment.To);
+            return boundarySegment;
         }
     }
 }
