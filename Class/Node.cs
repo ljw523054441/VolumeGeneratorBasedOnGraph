6d80e73c179d9f45ba876d6cd8dd6d75b59600d3
Garvey Lee <523054441@qq.com>
2021-11-15 21:15:39 +0800

    新增Node类，作为GraphNode和DualNode的父类

---
+++ Class/Node.cs
@@ -0,0 +1,27 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+
+namespace VolumeGeneratorBasedOnGraph.Class
+{
+    public class Node
+    {
+        public Point3d NodeVertex { get; set; }
+
+        public Node()
+        {
+
+        }
+
+        public Node(Node node)
+        {
+            this.NodeVertex = new Point3d(node.NodeVertex);
+        }
+
+        public Node(Point3d nodeVertex)
+        {
+            this.NodeVertex = nodeVertex;
+        }
+    }
+}
